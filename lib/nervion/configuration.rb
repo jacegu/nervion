8999d92a8958e421b9ed52428a7cdaea42b0e066
Javier Acero <j4cegu@gmail.com>
2012-05-04 22:00:11 +0200

    configuration settings are no longer stored as class instance variables

--- lib/nervion/configuration.rb
+++ lib/nervion/configuration.rb
@@ -2,36 +2,36 @@ module Nervion
   class Configuration
     UNCONFIGURED_SETTING = ''
 
-    def self.consumer_key=(consumer_key)
-      @consumer_key = consumer_key
-    end
+    attr_writer :consumer_key
 
-    def self.consumer_key
+    def consumer_key
       @consumer_key || UNCONFIGURED_SETTING
     end
 
-    def self.consumer_secret=(consumer_secret)
-      @consumer_secret = consumer_secret
-    end
+    attr_writer :consumer_secret
 
-    def self.consumer_secret
+    def consumer_secret
       @consumer_secret || UNCONFIGURED_SETTING
     end
 
-    def self.access_token=(access_token)
-      @access_token = access_token
-    end
+    attr_writer :access_token
 
-    def self.access_token
+    def access_token
       @access_token || UNCONFIGURED_SETTING
     end
 
-    def self.access_token_secret=(access_token_secret)
-      @access_token_secret = access_token_secret
+    attr_writer :access_token_secret
+
+    def access_token_secret
+      @access_token_secret || UNCONFIGURED_SETTING
     end
 
-    def self.access_token_secret
-      @access_token_secret || UNCONFIGURED_SETTING
+    def [](setting)
+      fetch setting
+    end
+
+    def fetch(setting)
+      send setting.to_sym
     end
   end
 end
--- spec/nervion/configuration_spec.rb
+++ spec/nervion/configuration_spec.rb
@@ -3,41 +3,47 @@ require 'nervion/configuration'
 describe Nervion::Configuration do
   context 'when it has not being configured' do
     it 'has an empty string as consumer_key' do
-      described_class.consumer_key.should eq ''
+      subject.consumer_key.should eq ''
     end
 
     it 'has an empty string as consumer secret' do
-      described_class.consumer_secret.should eq ''
+      subject.consumer_secret.should eq ''
     end
 
     it 'has an empty string as access token' do
-      described_class.access_token.should eq ''
+      subject.access_token.should eq ''
     end
 
     it 'has an empty string as access token secret' do
-      described_class.access_token_secret.should eq ''
+      subject.access_token_secret.should eq ''
     end
   end
 
   context 'configuration' do
     it 'can be set with the consumer key' do
-      described_class.consumer_key = 'consumer_key'
-      described_class.consumer_key.should eq 'consumer_key'
+      subject.consumer_key = 'consumer_key'
+      subject.consumer_key.should eq 'consumer_key'
     end
 
     it 'can be set with the consumer secret' do
-      described_class.consumer_secret = 'consumer_secret'
-      described_class.consumer_secret.should eq 'consumer_secret'
+      subject.consumer_secret = 'consumer_secret'
+      subject.consumer_secret.should eq 'consumer_secret'
     end
 
     it 'can be set with the access token' do
-      described_class.access_token = 'access token'
-      described_class.access_token.should eq 'access token'
+      subject.access_token = 'access token'
+      subject.access_token.should eq 'access token'
     end
 
     it 'can be set with the access token secret' do
-      described_class.access_token_secret = 'access token secret'
-      described_class.access_token_secret.should eq 'access token secret'
+      subject.access_token_secret = 'access token secret'
+      subject.access_token_secret.should eq 'access token secret'
     end
+  end
+
+  it 'can provide configuration settings as if it was hash' do
+    subject[:consumer_key].should eq ''
+    subject.consumer_secret = 'consumer_secret'
+    subject.fetch(:consumer_secret).should eq 'consumer_secret'
   end
 end
