3f46d97b27670b619c272d36b38737c47ace672a
Javier Acero <j4cegu@gmail.com>
2012-06-19 15:19:16 +0200

    some refactoring in step definitions

--- features/step_definitions/streaming_steps.rb
+++ features/step_definitions/streaming_steps.rb
@@ -1,6 +1,21 @@
 TEST_HOST = '0.0.0.0'
 TEST_PORT = '9000'
 
+def point_client_to_fake_server
+  Nervion.send :remove_const, 'STREAM_API_HOST'
+  Nervion.send :remove_const, 'STREAM_API_PORT'
+  Nervion.const_set 'STREAM_API_HOST', TEST_HOST
+  Nervion.const_set 'STREAM_API_PORT', TEST_PORT
+end
+
+def test_client_with(server_version)
+  EM.run do
+    EM.start_server(TEST_HOST, TEST_PORT, server_version)
+    EM.add_timer(0)   { Nervion.sample { |status| @statuses << status } }
+    EM.add_timer(0.1) { EM.stop }
+  end
+end
+
 Given /^the Twitter Streaming API is up$/ do
 end
 
@@ -10,16 +25,7 @@ end
 
 When /^a status update is sent by Twitter$/ do
   @statuses = []
-  Nervion.on_network_error { puts 'error but everything is ok' }
-  EM.run do
-    EM.start_server(TEST_HOST, TEST_PORT, WorkingStreamingApiDouble)
-    EM.add_timer(0.1) { Nervion.sample { |status| @statuses << status } }
-    EM.add_timer(0.2) { EM.stop }
-  end
-end
-
-Then /^Nervion calls the status callback with it$/ do
-  @statuses.count.should eq 100
+  test_client_with WorkingStreamingApiDouble
 end
 
 When /^an HTTP error occurs$/ do
@@ -27,19 +33,7 @@ When /^an HTTP error occurs$/ do
     @status, @body = status, body
     Nervion.stop
   end
-
-  Nervion.on_network_error { puts 'error but everything is ok' }
-
-  EM.run do
-    EM.start_server(TEST_HOST, TEST_PORT, HttpErrorStreamingApiDouble)
-    EM.add_timer(0)   { Nervion.sample { |status| @statuses << status } }
-    EM.add_timer(0.2) { EM.stop }
-  end
-end
-
-Then /^Nervion calls the HTTP error callback$/ do
-  @status.should eq 401
-  @body.should match /Unauthorized/
+  test_client_with HttpErrorStreamingApiDouble
 end
 
 When /^a network error occurs$/ do
@@ -47,21 +41,18 @@ When /^a network error occurs$/ do
     @network_error_detected = true
     Nervion.stop
   end
+  test_client_with NetworkErrorStreamingApiDouble
+end
 
-  EM.run do
-    EM.start_server(TEST_HOST, TEST_PORT, NetworkErrorStreamingApiDouble)
-    EM.add_timer(0)   { Nervion.sample { |status| @statuses << status } }
-    EM.add_timer(0.2) { EM.stop }
-  end
+Then /^Nervion calls the status callback with it$/ do
+  @statuses.count.should eq 100
+end
+
+Then /^Nervion calls the HTTP error callback$/ do
+  @status.should eq 401
+  @body.should match /Unauthorized/
 end
 
 Then /^Nervion calls the network error callback$/ do
   @network_error_detected.should be_true
-end
-
-def point_client_to_fake_server
-  Nervion.send :remove_const, 'STREAM_API_HOST'
-  Nervion.send :remove_const, 'STREAM_API_PORT'
-  Nervion.const_set 'STREAM_API_HOST', TEST_HOST
-  Nervion.const_set 'STREAM_API_PORT', TEST_PORT
 end
